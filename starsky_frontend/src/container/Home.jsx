a7ae49933d942e6923db9fb243ea290663391c43
rfkih <rifkihidayat23@yahoo.com>
2022-02-21 20:36:54 +0700

    sidebar toggle

--- starsky_frontend/src/container/Home.jsx
+++ starsky_frontend/src/container/Home.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import React, {useState, useRef, useEffect} from 'react'
+import {HiMenu} from 'react-icons/hi'
+import {AiFillCloseCircle } from 'react-icons/ai'
+import {Link, Route, Routes} from 'react-router-dom'
+
+import {Sidebar, UserProfile} from '../components'
+import {client } from '../client'
+import logo from '../assets/logo.png'
+import Pins from './Pins'
+import { userQuery } from '../utils/data'
+
 
 function Home() {
+
+  const [toggleSidebar, setToggleSidebar] = useState(false)
+  const [user, setUser] = useState(null)
+
+  const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear()
+
+  useEffect(() => {
+    const query = userQuery(userInfo?.googleId)
+    client.fetch(query)
+      .then((data) => {
+        setUser(data[0])
+      })
+  }, [])
+
   return (
-    <div>Home</div>
+    <div className='flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out'>
+      <div className='hidden md:flex h-screen flex-initial'>
+        <Sidebar user={user && user}/>
+      </div>
+      <div className='flex md:hidden flex-row'>
+          <HiMenu fontSize={40} className="cursor-pointer" onClick={()=> setToggleSidebar(true)}/>
+          <Link to="/">
+            <img src={logo} alt="logo" className="w-28"/>
+          </Link>
+          <Link to={`user-profile/${user?.id}`}>
+            <img src={user?.image} alt="logo" className="w-28"/>
+          </Link>
+      </div>
+      {toggleSidebar && (
+        <div className='fixed w-4/5 bg-white h-screen overflow-t-auto shadow-md z-10 animate-slide-in'>
+          <div className='absolute w-full flex justify-end items-center p-2'>
+            <AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={()=> {setToggleSidebar(false)}}/>
+          </div>
+          <Sidebar user={user && user} closeToggle={setToggleSidebar}/>
+        </div>
+      )}
+    </div>
   )
 }
 
