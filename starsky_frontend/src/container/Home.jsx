4e57f05708f0ce9bb7b8eeef160148be0d768655
rfkih <rifkihidayat23@yahoo.com>
2022-02-23 20:25:00 +0700

    delete button

--- starsky_frontend/src/container/Home.jsx
+++ starsky_frontend/src/container/Home.jsx
@@ -8,6 +8,7 @@ import {client } from '../client'
 import logo from '../assets/logo.png'
 import Pins from './Pins'
 import { userQuery } from '../utils/data'
+import { fetchUser } from '../utils/fetchUser'
 
 
 function Home() {
@@ -15,8 +16,7 @@ function Home() {
   const [toggleSidebar, setToggleSidebar] = useState(false)
   const [user, setUser] = useState(null)
   const scrollRef = useRef(null)
-
-  const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear()
+  const userInfo = fetchUser();
 
   useEffect(() => {
     const query = userQuery(userInfo?.googleId);
