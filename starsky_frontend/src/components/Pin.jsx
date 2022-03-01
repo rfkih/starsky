d1069b23ee37995d93a3967463c6d36a5fcde27b
rfkih <rifkihidayat23@yahoo.com>
2022-03-01 21:41:20 +0700

    last change

--- starsky_frontend/src/components/Pin.jsx
+++ starsky_frontend/src/components/Pin.jsx
@@ -16,7 +16,7 @@ function Pin({ pin: {postedBy, image, _id, destination, save}}) {
 
   const user = fetchUser();
 
-  const alreadySaved = !!(save?.filter((item) => item.postedBy._id === user.googleId))?.length;
+  const alreadySaved = !!(save?.filter((item) => item.postedBy._id === user?.googleId))?.length;
 
   const savePin = (id) => {
     if(!alreadySaved) {
@@ -26,10 +26,10 @@ function Pin({ pin: {postedBy, image, _id, destination, save}}) {
         .setIfMissing({save: [] } )
         .insert('after', 'save[-1]', [{
            _key: uuidv4(),
-           userId: user.googleId,
+           userId: user?.googleId,
            postedBy: {
               _type: 'postedBy',
-              _ref: user.googleId
+              _ref: user?.googleId
            }
         }])
         .commit()
